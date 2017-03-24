fb4aab1833273d1eae0d472ac33c27884da3e24c
pk <philipp@hackerbay.com>
2017-03-24 11:04:24 +0100

    initial commit

---
+++ js/mail.php
@@ -0,0 +1,41 @@
+<?php
+	if (isset($_POST["submit"])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$textarea1 = $_POST['textarea1'];
+		$human = intval($_POST['human']);
+		$from = 'phk.cc Contact Form'; 
+		$to = 'hello@phk.cc'; 
+		$subject = 'Message phk.cc Contact Form';
+		
+		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+ 
+		// Check if name has been entered
+		if (!$_POST['name']) {
+			$errName = 'Please enter your name';
+		}
+		
+		// Check if email has been entered and is valid
+		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Please enter a valid email address';
+		}
+		
+		//Check if message has been entered
+		if (!$_POST['textarea1']) {
+			$errTextarea1 = 'Please enter your message';
+		}
+		//Check if simple anti-bot test is correct
+		if ($human !== 5) {
+			$errHuman = 'Your anti-spam is incorrect';
+		}
+ 
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errTextarea1 && !$errHuman) {
+	if (mail ($to, $subject, $body, $from)) {
+		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+	} else {
+		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+	}
+}
+	}
+?>
